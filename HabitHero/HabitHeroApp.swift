335365ebf52a8563904cca84e428a2ef24f0b124
AhiyaBinal <107073852+AhiyaBinal@users.noreply.github.com>
2025-11-05 14:35:28 -0500

    Initial Commit

---
+++ HabitHero/HabitHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitHeroApp.swift
+//  HabitHero
+//
+//  Created by Binal Manek on 2025-11-05.
+//
+
+import SwiftUI
+
+@main
+struct HabitHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
