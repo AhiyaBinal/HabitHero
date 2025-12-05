3214080b78b8175a3929df73ce7372caf9b10cfe
AhiyaBinal <107073852+AhiyaBinal@users.noreply.github.com>
2025-12-05 15:41:33 -0500

    HH-44 Add habit button and Add Habit Screen added

---
+++ HabitHero/Views/SwiftUI/AddHabitView.swift
@@ -0,0 +1,18 @@
+//
+//  AddHabitView.swift
+//  HabitHero
+//
+//  Created by Binal Manek on 2025-12-05.
+//
+
+import SwiftUI
+
+struct AddHabitView: View {
+    var body: some View {
+        Text("You can add a new habit from here.")
+    }
+}
+
+#Preview {
+    AddHabitView()
+}
--- HabitHero/Views/SwiftUI/HabitListView.swift
+++ HabitHero/Views/SwiftUI/HabitListView.swift
@@ -10,56 +10,28 @@ internal import CoreData
 
 struct HabitListView: View {
 
-    @StateObject private var vm = HabitViewModel(service: HabitService())
-
-    @State private var newHabit = ""
-    @State private var updatedTitle = ""
+    @State private var showAddHabit = false
 
     var body: some View {
         NavigationView {
-            VStack {
-                HStack {
-                    TextField("Enter habit title", text: $newHabit)
-                        .textFieldStyle(.roundedBorder)
-
-                    Button("Add") {
-                        if !newHabit.isEmpty {
-                            vm.createHabit(title: newHabit)
-                            newHabit = ""
-                        }
-                    }
-                }
-                .padding()
-
-                List {
-                    ForEach(vm.habits, id: \.id) { habit in
-                        VStack(alignment: .leading) {
-                            Text(habit.title ?? "")
-                                .font(.headline)
-
-                            Text("\(habit.timestamp ?? Date(), formatter: dateFormatter)")
-                                .font(AppFont.title(10))
-                                .foregroundColor(.gray)
-                        }
-                    }
-                    .onDelete(perform: vm.deleteHabit)
-                }
+            VStack{
+                
             }
             .navigationTitle("Habits")
-            .onAppear {
-                vm.loadHabits()
+            .toolbar{
+                Button {
+                    showAddHabit = true
+                } label: {
+                    Image(systemName: "plus.circle.fill").font(.title2)
+                }
+                .accessibilityIdentifier("addHabitButton")
+            }
+            .sheet(isPresented: $showAddHabit) {
+                AddHabitView()
             }
         }
     }
 }
-
-let dateFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "MMM d, yyyy h:mm a"
-    return formatter
-}()
-
-
 #Preview {
     HabitListView()
 }
